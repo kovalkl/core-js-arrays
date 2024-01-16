00474cc406cb478ca800a6c0df95e12195d6dedd
Koval Anastasiia <ilovepokyshat@gmail.com>
2024-01-16 20:38:45 +0300

    feat: add propagateItemsByPositionIndex func

--- src/arrays-tasks.js
+++ src/arrays-tasks.js
@@ -534,8 +534,16 @@ function findLongestIncreasingSubsequence(/* nums */) {
  *  propagateItemsByPositionIndex([ 'a', 'b', 'c', null ]) => [ 'a', 'b', 'b', 'c', 'c', 'c',  null, null, null, null ]
  *  propagateItemsByPositionIndex([ 1,2,3,4,5 ]) => [ 1, 2, 2, 3, 3, 3, 4, 4, 4, 4, 5, 5, 5, 5, 5 ]
  */
-function propagateItemsByPositionIndex(/* arr */) {
-  throw new Error('Not implemented');
+function propagateItemsByPositionIndex(arr) {
+  const resultArr = [];
+  function repeatPush(el, index) {
+    if (index) {
+      resultArr.push(el);
+      repeatPush(el, index - 1);
+    }
+  }
+  arr.map((el, index) => repeatPush(el, index + 1));
+  return resultArr;
 }
 
 /**
